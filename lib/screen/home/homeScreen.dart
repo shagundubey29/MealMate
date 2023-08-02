484de54eeb9d1664de732bfa2e4e2acdc5c564bb
Shagun Dubey <1202shagun@gmail.com>
2023-08-03 03:23:14 +0530

    top bar added

--- lib/screen/home/homeScreen.dart
+++ lib/screen/home/homeScreen.dart
@@ -1,11 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
+import '../../util/appTheme.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor,
+      body: SafeArea(
+        child: Container(
+          child: Container(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          Text('Bangladesh'),
+                          Text('new Town'),
+                        ],
+                      ),
+                      Container(
+                        width: 45,
+                        height: 45,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: AppColors.primaryColorDark,
+                        ),
+                          child: const Icon(Icons.search),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/util/appTheme.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColors{
+  static final Color primaryColor = const Color(0xFFFEF6E1);
+  static final Color primaryColorDark = const Color(0xFFF3D3BC);
+  static final Color textColor = const Color(0xFF8f837f);
+  static final Color titleTextColor = const Color(0xFF412815);
+}
